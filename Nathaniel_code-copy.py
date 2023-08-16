4106c9d046c9709e487b42bd26ceacba8d462297
maulishashank <shashank.c.mauli@kipi.bi>
2023-08-17 01:42:10 +0530

    default param = 42.7

--- Nathaniel_code-copy.py
+++ Nathaniel_code-copy.py
@@ -42,7 +42,7 @@ def display(temp_list):
 
 
 @flow(name="weather_pipeline", retries=3)
-def pipeline(lat: float = 45.7):
+def pipeline(lat: float = 42.7):
     temp = fetch_many_weather(lat)
     result = save_weather(temp)
     display(temp)
